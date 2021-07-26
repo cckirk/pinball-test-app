52b9b4127c20fda8cdc34c4ef200a7638541e24e
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-26 12:57:13 -0700

    setting up API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
-  has_secure_password
+  #has_secure_password
+  #before_action :authenticate_user, except: :create
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def index
+    response = HTTP.get("http://localhost:3000/api/v1/locations.json")
+    render json: response.parse(:json)
+  end
 end
